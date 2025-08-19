51539d2b3b1e3e657d90c1a6600666c1e66eaa8f
tesnim01 <tesnimelbehi@gmail.com>
2025-08-19 20:52:44 +0100

    second  commit

--- index.js
+++ index.js
@@ -29,5 +29,5 @@ var listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 app.get('/api/whoami', function (req, res) {
-  res.json({ ipaddress: req.ip, language: req.headers['accept-language'], software: req.headers['user-agent'] });
+  res.json({ ipaddress: req.headers['x-forwarded-for'], language: req.headers['accept-language'], software: req.headers['user-agent'] });
 }); 
